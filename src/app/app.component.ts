f74e824fc5c13c28d354534904c5d36df89748fb
Vincent Snow <vincentsnow@gmx.com>
2017-10-13 14:25:45 -0700

    What's wrong here?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  emails = [
+		{email: "vincentsnow@gmx.com", importance: "Urgent", subject: "THERE'S NO MORE FUCKING TEA.", content: "There's no tea on the 3rd floor. Please buy some."},
+		{email:"rbell@codingdojo.com", importance: "Low", subject: "I DON'T HAVE AN INSIDE VOICE.", content: "WHAT THE HELL IS A QUIET???" }
+  ]
 }
